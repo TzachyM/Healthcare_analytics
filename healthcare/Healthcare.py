8235e58707c85e71b9195c7ac369c59b95ebadd1
Tzachy <tzachy11@gmail.com>
2021-03-15 11:58:48 +0200

    Healthcare project

---
+++ healthcare/Healthcare.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+
+def visual(train):
+    pass
+
+if __name__ == '__main__':
+    path = r'C:/Users/tzach/PycharmProjects/Healthcare analytics/healthcare/train_data.csv'
+    train = pd.read_csv(path)
+
+    nulls = train.columns[train.isna().any()]
+    for i in nulls:
+        print(f"In the columns {i}, we have {train[i].isna().sum()} NaN's")
