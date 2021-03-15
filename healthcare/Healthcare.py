2951fd3d74f1cd8719327e750e3b8814097e7b37
Tzachy <tzachy11@gmail.com>
2021-03-15 14:26:52 +0200

    Healthcare project

--- healthcare/Healthcare.py
+++ healthcare/Healthcare.py
@@ -1,7 +1,14 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
 
-
+def nan_processing(train):
+    nulls = train.columns[train.isna().any()]
+    for i in nulls:
+        print(f"In the columns {i}, we have {train[i].isna().sum()} NaN's")
+    train.dropna(axis=0, inplace=True)
+    print(train.isna().sum().sum(), 'NaN values remained after drop')
+    return train
 def visual(train):
     pass
 
@@ -9,6 +16,7 @@ if __name__ == '__main__':
     path = r'C:/Users/tzach/PycharmProjects/Healthcare analytics/healthcare/train_data.csv'
     train = pd.read_csv(path)
 
-    nulls = train.columns[train.isna().any()]
-    for i in nulls:
-        print(f"In the columns {i}, we have {train[i].isna().sum()} NaN's")
+    #Pre-processing
+    train = nan_processing(train)   #NaN
+    label = train.Stay  #label
+
