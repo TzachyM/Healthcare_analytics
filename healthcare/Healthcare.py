d9cfbc37f8480ddfc0027050d06c171964917936
Tzachy <tzachy11@gmail.com>
2021-03-15 20:55:19 +0200

    Healthcare project

--- healthcare/Healthcare.py
+++ healthcare/Healthcare.py
@@ -1,22 +1,42 @@
 import pandas as pd
 import numpy as np
 import seaborn as sns
+from sklearn.manifold import TSNE
+from sklearn import preprocessing
 
-def nan_processing(train):
-    nulls = train.columns[train.isna().any()]
+
+def nan_processing(df):
+    nulls = df.columns[df.isna().any()]
     for i in nulls:
-        print(f"In the columns {i}, we have {train[i].isna().sum()} NaN's")
-    train.dropna(axis=0, inplace=True)
-    print(train.isna().sum().sum(), 'NaN values remained after drop')
-    return train
+        print(f"In the column {i}, we have {df[i].isna().sum()} NaN's")
+    df.fillna(axis=0, inplace=True)
+    print(df.isna().sum().sum(), 'NaN values remained after drop')
+    return df
+
+
 def visual(train):
     pass
 
-if __name__ == '__main__':
-    path = r'C:/Users/tzach/PycharmProjects/Healthcare analytics/healthcare/train_data.csv'
-    train = pd.read_csv(path)
 
+if __name__ == '__main__':
+
+    path = r'C:/Users/tzach/PycharmProjects/Healthcare analytics/healthcare'
+    train = pd.read_csv(path + r'/train_data.csv')
+    test = pd.read_csv(path + r'/test_data.csv')
+    df = pd.concat(train, test)
     #Pre-processing
-    train = nan_processing(train)   #NaN
-    label = train.Stay  #label
+    df.drop(['case_id'], axis=1, inplace=True)
+    df = nan_processing(df).reset_index(drop=True)   #NaN
+    #label = train.Stay  #label
+    LE = preprocessing.LabelEncoder()
+    df = LE.fit_transform(y)[source]
+
+    for i in train.columns:
+        if type(i) != float or type(i) != int:
+            print(i)
+            #sns.barplot(x=i,y= hue='Stay', data=train)
+    #corrmat = train.corr()
+    #sns.heatmap(corrmat, square=True, annot=True)
+    #sns.jointplot(x='Age', y='Stay', data=train)
+
 
