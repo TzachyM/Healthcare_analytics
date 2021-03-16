fe6bf16bd40a3266648a556163d5bb633df4bcf0
Tzachy <tzachy11@gmail.com>
2021-03-16 16:42:33 +0200

    Healthcare project

--- healthcare/Healthcare.py
+++ healthcare/Healthcare.py
@@ -4,38 +4,15 @@ import seaborn as sns
 from sklearn.manifold import TSNE
 from sklearn import preprocessing
 
-
-def nan_processing(data, df):
-    print(data,':')
-    nulls = df.columns[df.isna().any()]
-    for i in nulls:
-        print(f"In the column {i}, we have {df[i].isna().sum()} NaN's")
-        df[i].fillna(df[i].median(), inplace=True)
-    print(df.isna().sum().sum(), 'NaN values remained after processing')
-    return df
-
-def cat_order(df):
-    col = ('Hospital_type_code', 'Age', 'family_size')
-    for c in col:
-        lbl = LabelEncoder()
-        lbl.fit(list(df[c].values))
-        df[c] = lbl.transform(list(df[c].values))
-    return df
-
-def visual(train):
-    pass
-
-
-if __name__ == '__main__':
-
+def load_data():
     path = r'C:/Users/tzach/PycharmProjects/Healthcare analytics/healthcare'
     train = pd.read_csv(path + r'/train_data.csv')
     test = pd.read_csv(path + r'/test_data.csv')
-    label = train.iloc[:, -1]
+    return train, test
 
-    # Pre-processing
-    # NaN (test and train came with NaN's)
-    train = nan_processing("train", train).reset_index(drop=True)
+
+def preprocessing(train, test):
+    train = nan_processing("train", train).reset_index(drop=True)     # NaN (test and train came with NaN's)
     stay_value_dict = {'0-10': 0, '11-20': 1, '21-30': 2, '31-40': 3, '41-50': 4, '51-60': 5, '61-70': 6, '71-80': 7,
                       '81-90': 8, '91-100': 9, 'More than 100 Days': 10}
     train.Stay = train.Stay.map(stay_value_dict)
@@ -52,15 +29,33 @@ if __name__ == '__main__':
     train_df = df.iloc[:len(train), :].reset_index(drop=True)
     test_df = df.iloc[len(train):, :].reset_index(drop=True)
     test_df.drop(['Stay'], axis=1, inplace=True)
-    g = sns.FacetGrid(train_df, col='Severity of Illness', hue='Stay')
-    g.map(sns.scatterplot, 'Visitors with Patient', 'Bed Grade', alpha=.7)
-    g.add_legend()
+
+
+def nan_processing(data, df):
+    print(data, ':')
+    nulls = df.columns[df.isna().any()]
+    for i in nulls:
+        print(f"In the column {i}, we have {df[i].isna().sum()} NaN's")
+        df[i].fillna(df[i].median(), inplace=True)
+    print(df.isna().sum().sum(), 'NaN values remained after processing')
+    return df
+
+
+def visual(train_df):
+    corrmat = train_df.corr()
+    sns.heatmap(corrmat, square=True, annot=True)
+    sns.barplot(x='Visitors with Patient', y='Stay', alpha=.7, data=train_df)
+
+
+if __name__ == '__main__':
+
+
+    train, test = load_data()
+    preprocessing(train, test)
+    visual(train_df)
+
 
     for i in train_df.select_dtypes(exclude=['object']):
         print(i)
-        #sns.barplot(x=i, y='Stay', data=train_df)
-    #corrmat = train_df.corr()
-    #sns.heatmap(corrmat, square=True, annot=True)
-    #sns.jointplot(x='Age', y='Stay', data=train_df)
 
 
